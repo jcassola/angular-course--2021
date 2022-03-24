aa294ce02aa61ac3d8a63b72931bbd56db8874db
Elodin <jcassola@uclv.cu>
2022-03-23 22:54:29 -0400

    one way data binding

--- src/app/heroes/heroe/heroe.component.ts
+++ src/app/heroes/heroe/heroe.component.ts
@@ -9,11 +9,18 @@ export class HeroeComponent{
   nombre: string = 'IronMan';
   edad: number = 45;
 
-  get nombreCapitalizado(){
+  get nombreCapitalizado(): string{
     return this.nombre.toUpperCase();
   }
 
   obtenerNombre(): string{
     return `${this.nombre} - ${this.edad}`;
+  }
+
+  cambiarNombre():void{
+    this.nombre = 'Spiderman';
+  }
+  cambiarEdad():void{
+    this.edad = 30;
   }
 }
