50bbbbb1ac94cdd2e1aeb6fd361cf4f9ca6f98a3
Elodin <jcassola@uclv.cu>
2022-03-26 15:21:06 -0400

    ngModel - two way data binding

--- src/app/dbz/main-page/main-page.component.ts
+++ src/app/dbz/main-page/main-page.component.ts
@@ -1,5 +1,10 @@
 import { Component } from '@angular/core';
 
+interface Personaje{
+  nombre: string,
+  poder: number
+}
+
 @Component({
   selector: 'app-main-page',
   templateUrl: './main-page.component.html'
@@ -7,8 +12,12 @@ import { Component } from '@angular/core';
 
 export class MainPageComponent {
 
+  nuevo: Personaje = {
+    nombre: 'Trucks',
+    poder: 14000
+  }
 
   agregar(){
-    console.log('Esta es una prueba');
+    console.log(this.nuevo);
   }
 }
